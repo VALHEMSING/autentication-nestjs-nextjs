3f6a2d3eb5e3ea2a54d25553698aa2de65be1ebe
Julian Quintero <juli.rivera.quintero@gmail.com>
2024-12-07 16:01:43 -0500

    feat(backend): Schema de user listo con sus propiedades

--- src/modules/user/schema/user.schema.ts
+++ src/modules/user/schema/user.schema.ts
@@ -1,2 +1,17 @@
+import { Document } from "mongoose";
+import { UserInterface } from "../interfaces/user.interface";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 
-export class User {}
+
+@Schema({ timestamps: true })
+export class User extends Document implements UserInterface {
+    @Prop({ required: true, unique: true })
+    username: string;
+    @Prop({ required: true, unique: true })
+    email: string;
+    @Prop({ required: true })
+    password: string;
+    @Prop({ default: true })
+    state: boolean;
+}
+export const UserSchema = SchemaFactory.createForClass(User);
