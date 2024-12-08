6b93fe70ca529079fbb2b91f109dc267262f6506
Julian Quintero <juli.rivera.quintero@gmail.com>
2024-12-07 23:34:17 -0500

    feat(backend): Module configurados para user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:270s7/nestjs')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:2707/nestjs')],
 
 })
 export class AppModule {}
