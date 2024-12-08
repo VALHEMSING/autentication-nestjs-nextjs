fc13456922668741ee1a3e933e120a6b18cecd46
Julian Quintero <juli.rivera.quintero@gmail.com>
2024-12-07 23:37:08 -0500

    feat(backend): Configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // Configuración de prefijo global
+   app.setGlobalPrefix('api');
+
+    // Configuración de Swagger
+  const config = new DocumentBuilder()
+  .setTitle('API')
+  .setDescription('API RESTFUL')
+  .setVersion('2.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
