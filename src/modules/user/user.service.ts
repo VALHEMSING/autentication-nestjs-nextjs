493c44c349ff7b9bebaef46557efd5abf246768e
Julian Quintero <juli.rivera.quintero@gmail.com>
2024-12-07 23:20:20 -0500

    feat(backend): Servicio de register para user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name)private userModel: Model<User>) {}
+  async register(user: CreateUserDto): Promise<User> {
+
+    const { username, email, password } = user;
+    const userExists = await this.userModel.findOne({ $or:[{username}, {email}] }).exec();
+
+    try {
+
+      if (userExists) 
+        throw new ConflictException(`El ${userExists.username === username ? 'username' : 'email'} en uso `);
+
+      const hashPassword = await bcrypt.hash(password, 10);
+      const newUser = new this.userModel({ 
+        ...user,
+        password: hashPassword
+       });
+      return newUser.save();
+    } catch (error) {
+      throw new InternalServerErrorException('Error al registrar el user.');
+    }
   }
 
   findAll() {
